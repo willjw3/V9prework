717383308354d0835bde220558f70bb88763038c
Will Ward <will.j.ward3@gmail.com>
2019-05-19 13:59:27 +0900

    changed display image and moved it to header

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import "bootstrap/dist/css/bootstrap.css"
 import Header from "./header"
 import "./layout.css"
 
