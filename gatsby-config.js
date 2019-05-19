57b32d6db985794a7f412d1b25088c1e8e1ac89b
Will Ward <will.j.ward3@gmail.com>
2019-05-20 01:35:55 +0900

    updated data fetch to get data from GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `meteorite-strikes`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p className="text-warning">Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `85px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import StrikeCard from "../components/StrikeCard"
+
+const IndexPage = ({data}) => {
+  const strikes = data.allStrike.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        {strikes.map(strike => {
+          return <StrikeCard
+            key={strike.node.id}
+            name={strike.node.name}
+            id={strike.node.id}
+            nametype={strike.node.nametype}
+            recclass={strike.node.recclass}
+            mass={strike.node.mass}
+            fall={strike.node.fall}
+            year={strike.node.year}
+            latitude={strike.node.latitude}
+            longitude={strike.node.longitude} />
+        })}
+      </div>
+      <div style={{ maxWidth: `85px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+         query StrikesQuery {
+           allStrike {
+             edges {
+               node {
+                 id
+                 fall
+                 latitude
+                 longitude
+                 mass
+                 name
+                 nametype
+                 recclass
+                 year
+               }
+             }
+           }
+         }
+       `
